4ec6b0a46303a88fb4cf838d76bd490841b7e083
Trey Gleason <s209058@student.aie.edu>
2022-03-03 09:45:50 -0800

    first working version of player controller

--- MajorProductionPreProduction/Assets/Scripts/PlayerControllerIsometric.cs
+++ MajorProductionPreProduction/Assets/Scripts/PlayerControllerIsometric.cs
@@ -10,8 +10,28 @@ public class PlayerControllerIsometric : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //movement
+
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         //deltaTime makes player move by 5 seconds instead of 5 frames
-        transform.Translate(input * speed * Time.deltaTime);
+        transform.Translate(input * speed * Time.deltaTime, Space.World);
+
+        //camera
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        //change player rotation
+        transform.rotation = Quaternion.Euler(new Vector3(0f, -angle, 0f));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 }
