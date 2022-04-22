f2ec1df6828ad240bf9fb7f1a90f611f815e2b1e
Trey Gleason <s209058@student.aie.edu>
2022-04-22 15:45:50 -0700

    I think that I got everything that is required for main menu Item display

--- MajorProductionPreProduction/Assets/Scripts/Collectables/DisplayRotation.cs
+++ MajorProductionPreProduction/Assets/Scripts/Collectables/DisplayRotation.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class DisplayRotation : MonoBehaviour
 {
+    public float speed = 1;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 90) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, -90) * Time.deltaTime * speed);
     }
 }
