02eac1658c5cae2ec6d92bfbe2f39cfe96f01500
Trey Gleason <s209058@student.aie.edu>
2022-04-29 11:31:28 -0700

    change the way fallzone works

--- MajorProductionPreProduction/Assets/Scripts/Player/PlayerFellOutOfMap.cs
+++ MajorProductionPreProduction/Assets/Scripts/Player/PlayerFellOutOfMap.cs
@@ -7,7 +7,8 @@ public class PlayerFellOutOfMap : MonoBehaviour
     public Rigidbody player;
     private PlayerControllerIsometric playerControllerScript;
 
-    public Vector3 movePlayer;
+    public Transform respawnpoint;
+    //public Vector3 movePlayer;
 
     //public float movePlayerY = 2;
     //public float movePlayerX = 2;
@@ -26,7 +27,7 @@ public class PlayerFellOutOfMap : MonoBehaviour
             playerControllerScript.enabled = false;
 
             player = player.GetComponent<Rigidbody>();
-            player.position = new Vector3(player.position.x + movePlayer.x, player.position.y + movePlayer.y, player.position.z + movePlayer.z);
+            player.position = respawnpoint.position;
 
             playerControllerScript.enabled = true;
         }
